3343347d704d93db889ae2f96ebb1ec6e516d74c
Cayde Dixon <me@cazzar.net>
2021-05-10 01:49:09 +1000

    ..

--- config.plugin.zsh
+++ config.plugin.zsh
@@ -2,3 +2,10 @@ fpath=("${0:h}" $fpath)
 
 source ${0:A:h}/customization.zsh
 source ${0:A:h}/keybindings.zsh
+
+if [ -d "${0:A:h}/functions" ]; then
+    fpath=( "${0:A:h}/functions ""${fpath[@]}" )
+    for f in "${0:A:h}/functions/*"; do
+        autoload -Uz $(basename $f)
+    done
+fi
