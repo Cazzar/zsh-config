2b465dd9d7f8d3fa49e932de080727144f2994f0
Cayde Dixon <Cazzar@users.noreply.github.com>
2025-02-19 16:16:52 +1100

    Fixup an error in autoload

--- config.plugin.zsh
+++ config.plugin.zsh
@@ -4,8 +4,8 @@ source ${0:A:h}/customization.zsh
 source ${0:A:h}/keybindings.zsh
 
 if [ -d "${0:A:h}/functions" ]; then
-    fpath=( "${0:A:h}/functions ""${fpath[@]}" )
+    fpath=( "${0:A:h}/functions" "${fpath[@]}" )
     for f in ${0:A:h}/functions/*; do
-        autoload -Uz $(basename $f)
+        autoload -Uz "${f:t}"
     done
 fi
