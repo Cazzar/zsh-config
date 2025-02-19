b3ec3cc1af5f258497dc6df98e41f849054b9659
Cayde Dixon <Cazzar@users.noreply.github.com>
2025-02-19 12:35:02 +1100

    Update config, function loading should work now

--- config.plugin.zsh
+++ config.plugin.zsh
@@ -5,7 +5,7 @@ source ${0:A:h}/keybindings.zsh
 
 if [ -d "${0:A:h}/functions" ]; then
     fpath=( "${0:A:h}/functions ""${fpath[@]}" )
-    for f in "${0:A:h}/functions/*"; do
+    for f in ${0:A:h}/functions/*; do
         autoload -Uz $(basename $f)
     done
 fi
